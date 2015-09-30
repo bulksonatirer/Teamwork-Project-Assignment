5d5a7f3975cef97297e5a7021b535a10fee120bb
Georgi.Dimitrov.Hristov <georgi.dimitrov.hristov@gmail.com>
2015-10-01 01:33:23 +0300

    Replace labels with picture-boxes and make mouse hover.

--- AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.Designer.cs
+++ AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.Designer.cs
@@ -28,59 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.StartButton = new System.Windows.Forms.Label();
-            this.ScoreButton = new System.Windows.Forms.Label();
-            this.OptionsButton = new System.Windows.Forms.Label();
-            this.EndButton = new System.Windows.Forms.Label();
-            this.startPB = new System.Windows.Forms.PictureBox();
             this.StartMenu = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.startPB)).BeginInit();
+            this.start = new System.Windows.Forms.PictureBox();
+            this.exit = new System.Windows.Forms.PictureBox();
+            this.options = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.PictureBox();
+            this.help = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.StartMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.options)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.help)).BeginInit();
             this.SuspendLayout();
-            // 
-            // StartButton
-            // 
-            this.StartButton.Location = new System.Drawing.Point(687, 131);
-            this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(199, 59);
-            this.StartButton.TabIndex = 1;
-            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
-            // 
-            // ScoreButton
-            // 
-            this.ScoreButton.Location = new System.Drawing.Point(698, 254);
-            this.ScoreButton.Name = "ScoreButton";
-            this.ScoreButton.Size = new System.Drawing.Size(188, 71);
-            this.ScoreButton.TabIndex = 2;
-            this.ScoreButton.Click += new System.EventHandler(this.ScoreButton_Click);
-            // 
-            // OptionsButton
-            // 
-            this.OptionsButton.Location = new System.Drawing.Point(698, 364);
-            this.OptionsButton.Name = "OptionsButton";
-            this.OptionsButton.Size = new System.Drawing.Size(227, 73);
-            this.OptionsButton.TabIndex = 3;
-            this.OptionsButton.Click += new System.EventHandler(this.OptionsButton_Click);
-            // 
-            // EndButton
-            // 
-            this.EndButton.Location = new System.Drawing.Point(698, 476);
-            this.EndButton.Name = "EndButton";
-            this.EndButton.Size = new System.Drawing.Size(152, 68);
-            this.EndButton.TabIndex = 4;
-            this.EndButton.Click += new System.EventHandler(this.EndButton_Click);
-            // 
-            // startPB
-            // 
-            this.startPB.BackColor = System.Drawing.Color.Transparent;
-            this.startPB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.startPB.Image = global::DespicableMe_Memories.Properties.Resources.start;
-            this.startPB.Location = new System.Drawing.Point(690, 131);
-            this.startPB.Name = "startPB";
-            this.startPB.Size = new System.Drawing.Size(196, 59);
-            this.startPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.startPB.TabIndex = 5;
-            this.startPB.TabStop = false;
             // 
             // StartMenu
             // 
@@ -94,16 +54,81 @@
             this.StartMenu.TabIndex = 0;
             this.StartMenu.TabStop = false;
             // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Transparent;
+            this.start.Image = global::DespicableMe_Memories.Properties.Resources.start;
+            this.start.Location = new System.Drawing.Point(696, 144);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(235, 58);
+            this.start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.start.TabIndex = 5;
+            this.start.TabStop = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            this.start.MouseLeave += new System.EventHandler(this.start_MouseLeave);
+            this.start.MouseHover += new System.EventHandler(this.start_MouseHover);
+            // 
+            // exit
+            // 
+            this.exit.Image = global::DespicableMe_Memories.Properties.Resources.exit;
+            this.exit.Location = new System.Drawing.Point(696, 444);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(176, 56);
+            this.exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.exit.TabIndex = 6;
+            this.exit.TabStop = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            this.exit.MouseLeave += new System.EventHandler(this.exit_MouseLeave);
+            this.exit.MouseHover += new System.EventHandler(this.exit_MouseHover);
+            // 
+            // options
+            // 
+            this.options.Image = global::DespicableMe_Memories.Properties.Resources.options;
+            this.options.Location = new System.Drawing.Point(696, 342);
+            this.options.Name = "options";
+            this.options.Size = new System.Drawing.Size(313, 58);
+            this.options.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.options.TabIndex = 7;
+            this.options.TabStop = false;
+            this.options.Click += new System.EventHandler(this.options_Click);
+            this.options.MouseLeave += new System.EventHandler(this.options_MouseLeave);
+            this.options.MouseHover += new System.EventHandler(this.options_MouseHover);
+            // 
+            // score
+            // 
+            this.score.Image = global::DespicableMe_Memories.Properties.Resources.score;
+            this.score.Location = new System.Drawing.Point(696, 240);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(232, 58);
+            this.score.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.score.TabIndex = 8;
+            this.score.TabStop = false;
+            this.score.Click += new System.EventHandler(this.score_Click);
+            this.score.MouseLeave += new System.EventHandler(this.score_MouseLeave);
+            this.score.MouseHover += new System.EventHandler(this.score_MouseHover);
+            // 
+            // help
+            // 
+            this.help.Image = global::DespicableMe_Memories.Properties.Resources.help;
+            this.help.Location = new System.Drawing.Point(1073, 614);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(179, 55);
+            this.help.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.help.TabIndex = 9;
+            this.help.TabStop = false;
+            this.help.MouseLeave += new System.EventHandler(this.help_MouseLeave);
+            this.help.MouseHover += new System.EventHandler(this.help_MouseHover);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.startPB);
-            this.Controls.Add(this.EndButton);
-            this.Controls.Add(this.OptionsButton);
-            this.Controls.Add(this.ScoreButton);
-            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.options);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.start);
             this.Controls.Add(this.StartMenu);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1280, 720);
@@ -111,20 +136,25 @@
             this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MainForm";
             this.Text = "Despicable Me - Memories";
-            ((System.ComponentModel.ISupportInitialize)(this.startPB)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.StartMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.options)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.help)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox StartMenu;
-        private System.Windows.Forms.Label StartButton;
-        private System.Windows.Forms.Label ScoreButton;
-        private System.Windows.Forms.Label OptionsButton;
-        private System.Windows.Forms.Label EndButton;
-        private System.Windows.Forms.PictureBox startPB;
+        private System.Windows.Forms.PictureBox start;
+        private System.Windows.Forms.PictureBox exit;
+        private System.Windows.Forms.PictureBox options;
+        private System.Windows.Forms.PictureBox score;
+        private System.Windows.Forms.PictureBox help;
 
     }
 }
--- AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.cs
+++ AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DespicableMe_Memories.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,45 +19,25 @@ namespace DespicableMe_Memories
         {
             InitializeComponent();
             fixedStart = StartMenu;
-            //startPos = StartMenu.PointToClient(startPos);
-            
-            //StartButton.Parent = StartMenu;
-            //StartButton.Location = startPos;
-            //StartButton.BackColor = Color.Transparent;
 
-            var startPos = this.PointToScreen(StartButton.Location);
-            MakeButtonTrans(StartButton, startPos);
+            //--------PictureBox-Remove-Transparent--------\\
+            var startPos = this.PointToScreen(start.Location);          
+            MakeTransparent(start, startPos);
 
-            //scorePos = StartMenu.PointToClient(scorePos);
+            var exitPos = this.PointToScreen(exit.Location);
+            MakeTransparent(exit, exitPos);
 
-            //ScoreButton.Parent = StartMenu;
-            //ScoreButton.Location = scorePos;
-            //ScoreButton.BackColor = Color.Transparent;
+            var optionsPos = this.PointToScreen(options.Location);    //Наложи се да премахна всичките "label" защото след като сложа "PictureBox" върху тях 
+            MakeTransparent(options, optionsPos);                     //те не работят... А и проверих дали може да зададен "exit" дирекно на "PictureBox-a" 
+                                                                      // и става без проблеми :) 
+            var scorePos = this.PointToScreen(score.Location);
+            MakeTransparent(score, scorePos);
 
-            var scorePos = this.PointToScreen(ScoreButton.Location);
-            MakeButtonTrans(ScoreButton, scorePos);
-
-            //optionsPos = StartMenu.PointToClient(optionsPos);
-            
-            //OptionsButton.Parent = StartMenu;
-            //OptionsButton.Location = optionsPos;
-            //OptionsButton.BackColor = Color.Transparent;
-
-            var optionsPos = this.PointToScreen(OptionsButton.Location);
-            MakeButtonTrans(OptionsButton, optionsPos);
-
-            //endPos = StartMenu.PointToClient(endPos);
-
-            //EndButton.Parent = StartMenu;
-            //EndButton.Location = endPos;
-            //EndButton.BackColor = Color.Transparent;
-
-            var endPos = this.PointToScreen(EndButton.Location);
-            MakeButtonTrans(EndButton, endPos);
-
+            var helpPos = this.PointToScreen(help.Location);
+            MakeTransparent(help, helpPos);
         }
 
-        static public void MakeButtonTrans(Control button, System.Drawing.Point pos)
+        static public void MakeTransparent(Control button, System.Drawing.Point pos)
         {
             pos = fixedStart.PointToClient(pos);
             button.Parent = fixedStart;
@@ -64,33 +45,76 @@ namespace DespicableMe_Memories
             button.BackColor = Color.Transparent;
         }
 
-        private void StartButton_Click(object sender, EventArgs e)
+        private void start_Click(object sender, EventArgs e)
         {
-
-        }
-        //private void startPB_
-
-        private void ScoreButton_Click(object sender, EventArgs e)
-        {
-
+            
         }
 
-        private void OptionsButton_Click(object sender, EventArgs e)
+        private void score_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void EndButton_Click(object sender, EventArgs e)
+        private void options_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+
         }
 
-        //private void MainForm_Load(object sender, EventArgs e)
-        //{
-        //    StartMenu.Controls.Add(startPB);
-        //}
+        private void exit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
 
+            //----------Make-Mouse-Hover------------\\
+        private void start_MouseHover(object sender, EventArgs e)
+        {
+            start.Image = Resources.startShadow;                          // Наложи се картините да бъдат auto-size защото картинките с сянка ги взема дирекно
+        }                                                                // от "Resources" и не ги сляга с тяхния размер...Но мисля че и така не е лошо.
 
+        private void start_MouseLeave(object sender, EventArgs e)
+        {
+            start.Image = Resources.start;     
+        }
+
+        private void score_MouseHover(object sender, EventArgs e)
+        {
+            score.Image = Resources.scoreShadow;   
+        }
+
+        private void score_MouseLeave(object sender, EventArgs e)
+        {
+            score.Image = Resources.score;   
+        }
+
+        private void options_MouseHover(object sender, EventArgs e)
+        {
+            options.Image = Resources.optionsShadow;   
+        }
+
+        private void options_MouseLeave(object sender, EventArgs e)
+        {
+            options.Image = Resources.options;
+        }
+
+        private void exit_MouseHover(object sender, EventArgs e)
+        {
+            exit.Image = Resources.exitShadow;  
+        }
+
+        private void exit_MouseLeave(object sender, EventArgs e)
+        {
+            exit.Image = Resources.exit;  
+        }
+
+        private void help_MouseHover(object sender, EventArgs e)
+        {
+            help.Image = Resources.helpShadow;  
+        }
+
+        private void help_MouseLeave(object sender, EventArgs e)
+        {
+            help.Image = Resources.help;  
+        }
 
     }
 }
