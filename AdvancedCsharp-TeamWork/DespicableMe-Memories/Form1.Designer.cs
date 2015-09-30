93a9858b73e4dc414d88f175bda2b538d8cf361b
monitobeko <monitobeko@abv.bg>
2015-09-30 21:31:26 +0300

    Added button code

--- AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.Designer.cs
+++ AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.Designer.cs
@@ -28,14 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StartButton = new System.Windows.Forms.Label();
+            this.ScoreButton = new System.Windows.Forms.Label();
+            this.OptionsButton = new System.Windows.Forms.Label();
+            this.EndButton = new System.Windows.Forms.Label();
+            this.startPB = new System.Windows.Forms.PictureBox();
             this.StartMenu = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.startPB)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.StartMenu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(687, 131);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(199, 59);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ScoreButton
+            // 
+            this.ScoreButton.Location = new System.Drawing.Point(698, 254);
+            this.ScoreButton.Name = "ScoreButton";
+            this.ScoreButton.Size = new System.Drawing.Size(188, 71);
+            this.ScoreButton.TabIndex = 2;
+            this.ScoreButton.Click += new System.EventHandler(this.ScoreButton_Click);
+            // 
+            // OptionsButton
+            // 
+            this.OptionsButton.Location = new System.Drawing.Point(698, 364);
+            this.OptionsButton.Name = "OptionsButton";
+            this.OptionsButton.Size = new System.Drawing.Size(227, 73);
+            this.OptionsButton.TabIndex = 3;
+            this.OptionsButton.Click += new System.EventHandler(this.OptionsButton_Click);
+            // 
+            // EndButton
+            // 
+            this.EndButton.Location = new System.Drawing.Point(698, 476);
+            this.EndButton.Name = "EndButton";
+            this.EndButton.Size = new System.Drawing.Size(152, 68);
+            this.EndButton.TabIndex = 4;
+            this.EndButton.Click += new System.EventHandler(this.EndButton_Click);
+            // 
+            // startPB
+            // 
+            this.startPB.BackColor = System.Drawing.Color.Transparent;
+            this.startPB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.startPB.Image = global::DespicableMe_Memories.Properties.Resources.start;
+            this.startPB.Location = new System.Drawing.Point(690, 131);
+            this.startPB.Name = "startPB";
+            this.startPB.Size = new System.Drawing.Size(196, 59);
+            this.startPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.startPB.TabIndex = 5;
+            this.startPB.TabStop = false;
             // 
             // StartMenu
             // 
             this.StartMenu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.StartMenu.Image = global::DespicableMe_Memories.Properties.Resources.menu;
+            this.StartMenu.ImageLocation = "";
             this.StartMenu.Location = new System.Drawing.Point(0, 0);
             this.StartMenu.Name = "StartMenu";
             this.StartMenu.Size = new System.Drawing.Size(1264, 681);
@@ -48,11 +99,19 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.startPB);
+            this.Controls.Add(this.EndButton);
+            this.Controls.Add(this.OptionsButton);
+            this.Controls.Add(this.ScoreButton);
+            this.Controls.Add(this.StartButton);
             this.Controls.Add(this.StartMenu);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MainForm";
             this.Text = "Despicable Me - Memories";
+            ((System.ComponentModel.ISupportInitialize)(this.startPB)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.StartMenu)).EndInit();
             this.ResumeLayout(false);
 
@@ -61,6 +120,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox StartMenu;
+        private System.Windows.Forms.Label StartButton;
+        private System.Windows.Forms.Label ScoreButton;
+        private System.Windows.Forms.Label OptionsButton;
+        private System.Windows.Forms.Label EndButton;
+        private System.Windows.Forms.PictureBox startPB;
 
     }
 }
--- AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.cs
+++ AdvancedCsharp-TeamWork/DespicableMe-Memories/Form1.cs
@@ -12,9 +12,85 @@ namespace DespicableMe_Memories
 {
     public partial class MainForm : Form
     {
+        static PictureBox fixedStart;
+
         public MainForm()
         {
             InitializeComponent();
+            fixedStart = StartMenu;
+            //startPos = StartMenu.PointToClient(startPos);
+            
+            //StartButton.Parent = StartMenu;
+            //StartButton.Location = startPos;
+            //StartButton.BackColor = Color.Transparent;
+
+            var startPos = this.PointToScreen(StartButton.Location);
+            MakeButtonTrans(StartButton, startPos);
+
+            //scorePos = StartMenu.PointToClient(scorePos);
+
+            //ScoreButton.Parent = StartMenu;
+            //ScoreButton.Location = scorePos;
+            //ScoreButton.BackColor = Color.Transparent;
+
+            var scorePos = this.PointToScreen(ScoreButton.Location);
+            MakeButtonTrans(ScoreButton, scorePos);
+
+            //optionsPos = StartMenu.PointToClient(optionsPos);
+            
+            //OptionsButton.Parent = StartMenu;
+            //OptionsButton.Location = optionsPos;
+            //OptionsButton.BackColor = Color.Transparent;
+
+            var optionsPos = this.PointToScreen(OptionsButton.Location);
+            MakeButtonTrans(OptionsButton, optionsPos);
+
+            //endPos = StartMenu.PointToClient(endPos);
+
+            //EndButton.Parent = StartMenu;
+            //EndButton.Location = endPos;
+            //EndButton.BackColor = Color.Transparent;
+
+            var endPos = this.PointToScreen(EndButton.Location);
+            MakeButtonTrans(EndButton, endPos);
+
         }
+
+        static public void MakeButtonTrans(Control button, System.Drawing.Point pos)
+        {
+            pos = fixedStart.PointToClient(pos);
+            button.Parent = fixedStart;
+            button.Location = pos;
+            button.BackColor = Color.Transparent;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        //private void startPB_
+
+        private void ScoreButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OptionsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EndButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //private void MainForm_Load(object sender, EventArgs e)
+        //{
+        //    StartMenu.Controls.Add(startPB);
+        //}
+
+
+
     }
 }
